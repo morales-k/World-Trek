fa38002a962aa7da3ad164796709aa1e10a53917
Krista M <morales-k@outlook.com>
2023-04-26 17:54:57 -0400

    Player Updates
    Updated positioning. movePlayer now returns updated player coords.

--- Scripts/player.js
+++ Scripts/player.js
@@ -1,20 +1,43 @@
 import { SphereGeometry, MeshLambertMaterial, Mesh } from "three";
 
-export const createPlayer = (envmap, texture, position) => {
-  const px = Math.random() * 0.04;
-  const pz = Math.random() * 0.04;
+export const createPlayer = (texture, position) => {
   const geo = new SphereGeometry(1, 16, 16);
   
-  geo.translate(position[0] + px, 2, position[1] + pz);
+  geo.translate(position[0], 2, position[1]);
 
   const mesh = new Mesh(
     geo,
     new MeshLambertMaterial({ map: texture })
   );
+  // Name object for easy removal when updating movement.
+  mesh.name = "player";
 
   return mesh;
 };
 
-export const movePlayer = (key) => {
-  console.log(key);
+export const movePlayer = (key, playerCoords) => {
+  let updatedPlayerCoords = [...playerCoords];
+
+  switch (key) {
+    case "ArrowUp": {
+      updatedPlayerCoords[0] -= 1;
+      break;
+    }
+    case "ArrowDown": {
+      updatedPlayerCoords[0] += 1;
+      break;
+    }
+    case "ArrowLeft": {
+      updatedPlayerCoords[1] += 1;
+      break;
+    }
+    case "ArrowRight": {
+      updatedPlayerCoords[1] -= 1;
+      break;
+    }
+    default:
+      break;
+  }
+
+  return updatedPlayerCoords;
 };
